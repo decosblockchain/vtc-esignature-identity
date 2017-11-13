d8cef06eedcb8acd2e830bbc58cc48a0c91aaea2
Gert-Jaap Glasbergen <gertjaap@decoscrypto.com>
2017-11-13 15:29:06 +0100

    Added sending of transaction

--- index.js
+++ index.js
@@ -3,13 +3,68 @@ var sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 const uuidv4 = require('uuid/v4');
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const mongoConnector = require('./mongo').MongoConnector;
 const app = express();
+const async = require('async');
 const fs = require('fs');
 const path = require('path');
 var templateHtml = fs.readFileSync(path.join(__dirname,"confirm-mail.html"));
+var vertcoinTestnet = {
+    messagePrefix: 'Vertcoin Signed Message:\n',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    pubKeyHash: 0x4A,
+    scriptHash: 0xC4,
+    wif: 0xEF
+};
+var keyPair = bitcoin.ECPair.fromWIF(process.env.WALLET_PRIVATE_KEY, vertcoinTestnet);
+var address = keyPair.getAddress();
+console.log("Using wallet: ", address);
+
+var backendServer = process.env.BACKEND_SERVER;
 app.use(bodyParser.json());
+var transactionCost = 99900;
+
+var sendIdentityTransaction = function(payload, callback) {
+    request(backendServer + "addressTxos/" + address, { json : true }, (error, response, result) => {
+        var tx = new bitcoin.TransactionBuilder(vertcoinTestnet);
+        var total = 0;
+        result.forEach((txo) => {
+            if(txo.spender == null) {
+                if(total <= transactionCost) {
+                    total += txo.value;
+                    tx.addInput(txo.txhash,txo.vout);
+                }
+            }
+        });
+        tx.addOutput(address, total-transactionCost-100);
+        tx.addOutput(payload.targetAddress, 100);
+        var ret = bitcoin.script.compile(
+            [
+              bitcoin.opcodes.OP_RETURN,
+              new Buffer("IDEN")
+            ])
+        tx.addOutput(ret, 0);
+        var ret = bitcoin.script.compile(
+            [
+              bitcoin.opcodes.OP_RETURN,
+              bitcoin.crypto.sha256(payload.identity.toLowerCase())
+            ])
+        tx.addOutput(ret, 0);
+        tx.sign(0, keyPair);
+        var txData = tx.build().toHex();
+        request.post({url : backendServer + "sendRawTransaction", body : txData }, (error, response, result) => {
+            console.log("Sent identity tx:", result);
+            callback();
+        });
+    });
+}
+var sendQueue = async.queue(sendIdentityTransaction,1)
+
 app.post('/identify', (req, res) => {
     mongoConnector.connectFromController(res, (db) => {
         var guid = uuidv4();
@@ -40,9 +95,7 @@ app.post('/identify', (req, res) => {
                   console.error(err);
                 } 
             });
-
-        
-              
+             
             res.sendStatus(204);
         });
         
@@ -53,7 +106,7 @@ app.post('/identify', (req, res) => {
 app.get('/confirm', (req, res) => {
     mongoConnector.connectFromController(res, (db) => {
     
-        db.collection("identity-requests").findOne({ id : req.query.id }, (err, result) => {
+        db.collection("identity-requests").findOne({ id : req.query.id }, (err, identityRecord) => {
             if(err) { 
                 res.sendStatus(500);
                 return;
@@ -64,10 +117,15 @@ app.get('/confirm', (req, res) => {
                     res.sendStatus(500);
                     return;
                 }
+
+                sendQueue.push({identity:identityRecord.email, targetAddress: identityRecord.address});
+
                 res.sendFile(path.join(__dirname,"confirm.html"));
             });
         });
     });
 });
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
